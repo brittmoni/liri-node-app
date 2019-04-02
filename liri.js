d5db5e40d5eff908f906223858a4d1cd428cbd70
Brittany F <brittfranklin92@gmail.com>
2019-04-01 23:38:58 -0400

    Updates to Liri

--- liri.js
+++ liri.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 var Spotify = require('node-spotify-api');
 var keys = require('./keys');
-var Music = require('./spotify');
+// var Music = require('./spotify');
 var Movies = require('./movies');
+var axios = require('axios');
 
 var spotify = new Spotify(keys.spotify);
 var music = new Music();
@@ -32,3 +33,53 @@ switch(search) {
     console.log('Please enter "concert-this", "spotify-this-song", "movie-this", or "do-what-it-says"');
     break;
 }
+
+function Music() {
+  this.musician = function(artist) {
+    var URL = 'https//rest.bandsintown.com/artists/' + artist + '/events?app_id=' + spotify;
+
+    axios
+      .get(URL)
+      .then(function(err, response) {
+        var results = response.data[0];
+
+        var artistEvents = `
+      Venue Name: ${results.venue.name}
+      Venue Location: ${results.venue.city}
+      Event Date: ${results.datetime}
+      ----------------------------------------
+      `
+
+      if (err) {
+        console.log(err);
+      }
+
+      console.log(artistEvents);
+      })
+  }
+  
+    this.song = function(songName){
+    spotify.search({ type: 'track', query: songName, limit: 7 }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      } else {
+        var songData = data.tracks.items;
+
+        songData.forEach(function(result) {
+          console.log(`
+      Artist(s): ${songData.artists[0].name}
+      Song Name: ${songData.name}
+      Link: ${songData.preview_url}
+      Album: ${songData.album.name}
+      ---------------------------------------`);
+        });
+      }
+    
+      if(!songName) {
+        songName = 'The Sign';
+      } 
+    });
+  }
+}
+
+
